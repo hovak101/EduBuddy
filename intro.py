36ad51db90e205392bf13683ab42188144bc62f9
Onur <onuryararbas@gmail.com>
2023-06-17 10:59:45 -0700

    Tinkering with API

--- intro.py
+++ intro.py
@@ -1,2 +1,16 @@
-import numpy as np
-import openai as oa
+import openai
+import os
+
+# Example OpenAI Python library request
+MODEL = "gpt-4"
+openai.api_key = "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"
+response = openai.ChatCompletion.create(
+    model=MODEL,
+    messages=[
+        {"role": "user", "content": "What is the weather in San Jose, California?"},
+        
+    ],
+    temperature=0.2,
+)
+
+print(response['choices'][0]['message']['content'])
