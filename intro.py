5ab3d437e5c0a773f2fdb53c88e1f935c776347f
Onur <onuryararbas@gmail.com>
2023-06-17 11:33:41 -0700

    Working quiz-creating model using Chat-GPT-4 Model

--- intro.py
+++ intro.py
@@ -1,14 +1,19 @@
 import openai
 import os
+from dotenv import load_dotenv, find_dotenv
 
 # Example OpenAI Python library request
 MODEL = "gpt-4"
-openai.api_key = ""
+_ = load_dotenv("keys.env")
+openai.api_key = os.environ['OPENAI_API_KEY']
 response = openai.ChatCompletion.create(
     model=MODEL,
-    prompt = "Explain the golden ratio?",
+
+    #decide which system, assistant to use.
+
     messages=[
-        {"role": "user", "content": "Hello!"},
+        {"role": "assistant", "content": "You are someone that creates question on a given topic"},
+        {"role": "user", "content": "Can you create 10 questions on the American Revolutionary War with the answer to each question"},
     ],
     temperature=0.2
 )
