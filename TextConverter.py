7862ee52648e2e330946f51c7ec2f3828b34ff91
Alex Hovakimyan <hovak101@gmail.com>
2023-06-17 22:51:33 -0700

    Added functions for retrieving text and generating output for it.

--- TextConverter.py
+++ TextConverter.py
@@ -2,15 +2,103 @@ import os
 import openai
 from dotenv import load_dotenv, find_dotenv
 from llama_index import VectorStoreIndex, SimpleDirectoryReader
+import pyperclip
 
-def convertFileToVector(file):
-    _ = load_dotenv(r'key.env')
+MODEL = "gpt-4"
+
+def waitAndReturnNewText():
+    clipboard = pyperclip.waitForNewPaste()
+    return clipboard
+
+def getTitleFromText(text):
+    _ = load_dotenv(r'assistant-user-model\keys.env')
+    openai.api_key = os.environ['OPENAI_API_KEY']
+    response = openai.ChatCompletion.create(
+        model=MODEL,
+
+        #decide which system, assistant to use.
+        # input from user data, yo uask to summarize, it will put assistant as "you are a summarizer..."
+
+        messages=[
+            {"role": "assistant", "content": "You are someone that generate titles given texts."},
+            {"role": "user", "content": "Given the following code, generate a title from 0 to 19 characters: " + text},
+        ],
+        temperature=0
+    )
+
+    return response['choices'][0]['message']['content']
+    
+def generateSummaryFromText(text, minimumWords, maximumWords):
+    _ = load_dotenv(r'assistant-user-model\keys.env')
+    openai.api_key = os.environ['OPENAI_API_KEY']
+    response = openai.ChatCompletion.create(
+        model=MODEL,
+
+        #decide which system, assistant to use.
+        # input from user data, yo uask to summarize, it will put assistant as "you are a summarizer..."
+
+        messages=[
+            {"role": "assistant", "content": "You are someone that summarizes information on a given topic."},
+            {"role": "user", "content": "Summarize the following information in " + minimumWords + " to " + maximumWords + " words: " + text},
+        ],
+        temperature=0
+    )
+
+    return response['choices'][0]['message']['content']
+
+def generateQuizFromText(text, numOfQuestions):
+    _ = load_dotenv(r'assistant-user-model\keys.env')
+    openai.api_key = os.environ['OPENAI_API_KEY']
+    response = openai.ChatCompletion.create(
+        model=MODEL,
+
+        #decide which system, assistant to use.
+
+        messages=[
+            {"role": "assistant", "content": "You are someone that creates question on a given topic"},
+            {"role": "user", "content": "Create " + numOfQuestions + " questions based off of the following text: " + text},
+        ],
+        temperature=0
+    )
+
+    return response['choices'][0]['message']['content']
+
+def generateResponseFromFile(file, query):
+    _ = load_dotenv(r'assistant-user-model\keys.env')
     openai.api_key = os.environ['OPENAI_API_KEY']
     # Load data from a file
-    documents = SimpleDirectoryReader(file).load_data()
+    documents = SimpleDirectoryReader(input_files=[file]).load_data()
     # Create an index from the loaded documents
     index = VectorStoreIndex.from_documents(documents)
 
-convertFileToVector(r'content')
+    query_engine = index.as_query_engine()
+    res = query_engine.query(query)
+    return res
 
-# find python module that shows you which applications your using.
+def getResponseLengthFromText(text):
+    length = len(text)
+
+    if length < 50:
+        return length
+    
+    if length < 1000:
+        return length // 5
+    
+    else:
+        return 200; 
+
+# def understand(res):
+#   response = openai.ChatCompletion.create(
+#       model='gpt-4',
+
+#       #decide which system, assistant to use.
+
+#       messages=[
+#           {"role": "system", "content": "You make summary from a text input"},
+#           {"role": "assistant", "content": "Summary with bullet points"},
+#           {"role": "user", "content": f"{res}"},
+#       ],
+#       temperature=0
+#   )
+#   return response
+# print(generateResponseFromFile(r'content/CaseForReperations.docx', 'Generate a ten-question quiz from the input information'))
---
+++ requiredLibraries.py
@@ -0,0 +1,19 @@
+import subprocess
+import sys
+
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+# For reading in various file types: 
+# For reading pdfs
+install('pypdf')
+
+# For reading word documents
+install('docx2txt')
+
+# # For image to text generation
+# install('torch transformers sentencepiece Pillow')
+# install('protobuf')
+
+install('pygetwindow')
+install('pywinauto')
