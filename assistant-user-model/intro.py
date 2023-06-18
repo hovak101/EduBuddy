3e98eec4e0b146befc89f43fda959be579ec69d7
Onur <onuryararbas@gmail.com>
2023-06-17 22:15:14 -0700

    agent-model development

--- assistant-user-model/intro.py
+++ assistant-user-model/intro.py
@@ -4,19 +4,19 @@ from dotenv import load_dotenv, find_dotenv
 
 # Example OpenAI Python library request
 MODEL = "gpt-4"
-_ = load_dotenv("assistant-user-model/keys.env")
+_ = load_dotenv("keys.env")
+
+assistantCont = input("What am I?\t")
+userCont = input("What would you like me to do?\t")
 openai.api_key = os.environ['OPENAI_API_KEY']
 response = openai.ChatCompletion.create(
     model=MODEL,
-
-    #decide which system, assistant to use.
-
     messages=[
-        {"role": "system", "content": "You make history questions"},
-        {"role": "assistant", "content": "You are someone that creates question on a given topic"},
-        {"role": "user", "content": "Can you create 10 questions on the American Revolutionary War with the answer to each question"},
+        {"role": "system", "content": "You are a very helpful assistant"},
+        {"role": "assistant", "content": assistantCont},
+        {"role": "user", "content": userCont},
     ],
-    temperature=0
+    temperature=0.2
 )
 
 print(response['choices'][0]['message']['content'])
